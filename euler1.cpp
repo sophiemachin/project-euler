b72110e3df826e3b637a3e32c750622751ccd273
sophiemachin <sophiemachin@gmail.com>
2019-05-25 17:53:55 +0100

    Add problem description, clean up white space

--- euler1.cpp
+++ euler1.cpp
@@ -1,6 +1,8 @@
 //
 //  main.cpp
 //  Euler problem 1
+//  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//  Find the sum of all the multiples of 3 or 5 below 1000.
 //
 //  Created by Sophie Machin on 05/01/2015.
 //  Copyright (c) 2015 Sophie Machin. All rights reserved.
@@ -18,7 +20,8 @@ int main()
     int  l = 0; //current term
     int  total = 0;
     
-    for(i=0; i<10000; i++) // arbitrarily choose for-loop to be 10,000 to catch all terms under 4 million
+    // arbitrarily choose for-loop to be 10,000 to catch all terms under 4 million
+    for(i=0; i<10000; i++) 
     {
         l = j + k;
         if(l%2==0){
@@ -27,12 +30,7 @@ int main()
         k=l; // updating values
         if(l>4000000) // if the total exceeds 4 million, break out of for-loop
             break;
-
-    }
-    
+    } 
     cout << "Total is  " << total +2 << endl;
-    
-    
 
-    
 }
